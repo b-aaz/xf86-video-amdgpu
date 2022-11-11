b-aaz/xf86-video-amdgpu

commit e39a3ee07c9dea73b0452b71b1ef633b6cd6f389

Prefer crtc of primary output for synchronization when screen has to crtcs with the same coverage
This is adjusting randr_crtc_covering_drawable to cover scenario fixed in
9151f3b1c2ebcc34e63195888ba696f2183ba5e2

Signed-off-by: Łukasz Spintzyk <lukasz.spintzyk@synaptics.com>
Signed-off-by: Shashank Sharma <contactshashanksharma@gmail.com>
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

src/amdgpu_video.c
@@ -172,13 +172,16 @@ static RRCrtcPtr
 amdgpu_crtc_covering_box(ScreenPtr pScreen, BoxPtr box, Bool screen_is_xf86_hint)
 {
 	rrScrPrivPtr pScrPriv;
-	RRCrtcPtr crtc, best_crtc;
+	RRCrtcPtr crtc, best_crtc, primary_crtc;
 	int coverage, best_coverage;
 	int c;
 	BoxRec crtc_box, cover_box;
+	RROutputPtr primary_output;
 
 	best_crtc = NULL;
 	best_coverage = 0;
+	primary_crtc = NULL;
+	primary_output = NULL;
 
 	if (!dixPrivateKeyRegistered(rrPrivKey))
 		return NULL;
@@ -187,6 +190,10 @@ amdgpu_crtc_covering_box(ScreenPtr pScreen, BoxPtr box, Bool screen_is_xf86_hint
 
 	if (!pScrPriv)
 		return NULL;
+
+	primary_output = RRFirstOutput(pScreen);
+	if (primary_output && primary_output->crtc)
+		primary_crtc = primary_output->crtc->devPrivate;
 
 	for (c = 0; c < pScrPriv->numCrtcs; c++) {
 		crtc = pScrPriv->crtcs[c];
@@ -198,7 +205,8 @@ amdgpu_crtc_covering_box(ScreenPtr pScreen, BoxPtr box, Bool screen_is_xf86_hint
 		amdgpu_crtc_box(crtc, &crtc_box);
 		amdgpu_box_intersect(&cover_box, &crtc_box, box);
 		coverage = amdgpu_box_area(&cover_box);
-		if (coverage > best_coverage) {
+		if (coverage > best_coverage ||
+		   (crtc == primary_crtc && coverage == best_coverage)) {
 			best_crtc = crtc;
 			best_coverage = coverage;
 		}

commit 2ec854d48e0e44fc60c3955663f700cbefea3553

Fix primary output handling in amdgpu_crtc_covering_box().
Commit e39a3ee07c9dea73b0452b71b1ef633b6cd6f389
tries to reintroduce the RandR primary output as a tie breaker
in amdgpu_crtc_covering_box(), but that function wrongly
assigns a void* devPrivate, which is actually a xf86CrtcPtr,
to the RRCrtcPtr primary_crtc, a pointer target type mismatch!

This causes a later pointer comparison of primary_crtc with
RRCrtcPtr crtc to always fail, so that the user selected
primary output can not ever successfully act as a tie-breaker
when multiple candidate crtcs cover the same box area,
defeating the whole purpose of that commit! Not sure how
this failure could have ever evaded any basic testing.

Fix this trivially by assigning the right variable.

Successfully tested on a multi-display setup, verifying
that the primary output now works as tie breaker as
intended.

Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
Fixes: e39a3ee07c9d ("Prefer crtc of primary output for synchronization when screen has to crtcs with the same coverage")

src/amdgpu_video.c
@@ -126,7 +126,7 @@ amdgpu_crtc_covering_box(ScreenPtr pScreen, BoxPtr box, Bool screen_is_xf86_hint
 
 	primary_output = RRFirstOutput(pScreen);
 	if (primary_output && primary_output->crtc)
-		primary_crtc = primary_output->crtc->devPrivate;
+		primary_crtc = primary_output->crtc;
 
 	for (c = 0; c < pScrPriv->numCrtcs; c++) {
 		crtc = pScrPriv->crtcs[c];