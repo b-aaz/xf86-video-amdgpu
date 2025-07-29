6e65547971a8839ed9b2aab27205daa094aedb6a
Enrico Weigelt, metux IT consult <info@metux.net>
2025-07-29 14:44:58 +0200

    configure.ac: drop check for GBM_BO_USE_FRONT_RENDERING
    It's present since at least mesa 22.3. No need to care about ancient
    mesa versions anymore.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- src/amdgpu_bo_helper.c
+++ src/amdgpu_bo_helper.c
@@ -82,10 +82,8 @@ struct amdgpu_buffer *amdgpu_alloc_pixmap_bo(ScrnInfoPtr pScrn, int width,
 		if (usage_hint & AMDGPU_CREATE_PIXMAP_SCANOUT)
 			bo_use |= GBM_BO_USE_SCANOUT;
 
-#ifdef HAVE_GBM_BO_USE_FRONT_RENDERING
 		if (usage_hint & AMDGPU_CREATE_PIXMAP_FRONT)
 			bo_use |= GBM_BO_USE_FRONT_RENDERING;
-#endif
 
 		if (usage_hint == CREATE_PIXMAP_USAGE_SHARED ||
 		    (usage_hint & AMDGPU_CREATE_PIXMAP_LINEAR)) {
